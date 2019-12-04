1a6bac6160350176f89010367e437322d1239ec9
zhuzichu <coffee.zhu@jollycorp.com>
2019-12-04 09:23:05 +0800

    update

---
+++ app/src/main/kotlin/com/zhuzichu/android/wan/ui/opencv/main/viewmodel/ViewModelOpencv.kt
@@ -0,0 +1,28 @@
+package com.zhuzichu.android.wan.ui.opencv.main.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.zhuzichu.android.shared.base.ViewModelAnalyticsBase
+import com.zhuzichu.android.shared.extension.map
+import com.zhuzichu.android.wan.BR
+import com.zhuzichu.android.wan.R
+import me.tatarka.bindingcollectionadapter2.itembindings.OnItemBindClass
+
+import javax.inject.Inject
+
+class ViewModelOpencv @Inject constructor() : ViewModelAnalyticsBase() {
+
+    companion object {
+        const val TYPE_GRAY = 0
+    }
+
+    val items = MutableLiveData<List<Any>>().apply {
+        value = listOf(
+            ItemViewModelOpencv(this@ViewModelOpencv, TYPE_GRAY, R.string.gray)
+        )
+    }
+
+    val itemBinding = OnItemBindClass<Any>().apply {
+        map<ItemViewModelOpencv>(BR.item, R.layout.item_opencv)
+    }
+
+}
