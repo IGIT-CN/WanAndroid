d80b3f3d6c9dc5438fadaa55a019b670a2a21e60
zhuzichu <zhuzichu520@gmail.com>
2020-02-27 15:38:33 +0800

    update

--- wan_flutter/.android/app/src/main/java/com/zhuzichu/android/flutter/host/MainActivity.java
+++ wan_flutter/.android/app/src/main/java/com/zhuzichu/android/flutter/host/MainActivity.java
@@ -1,7 +1,7 @@
 package com.zhuzichu.android.flutter.host;
 
-import android.os.Bundle;
 import androidx.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
